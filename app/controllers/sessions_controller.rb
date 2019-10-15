07718c6de5ceea377065a7c1a606e97bf24b010a
Adrian Titus <atitus@icess.us>
2019-10-14 21:33:49 -0400

    fix token save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   def create
     session[:auth_hash] = request.env['omniauth.auth']
-    Token.first_or_create(access_token: session['auth_hash']['credentials']['token'], refresh_token: session['auth_hash']['credentials']['refresh_token'])
+    t=Token.first_or_create
+    t.update(access_token: session['auth_hash']['credentials']['token'], refresh_token: session['auth_hash']['credentials']['refresh_token'])
+
     redirect_to '/'
   end
 
